f2bfbaa448204cd7779be7ee8e2297e1e8a99f99
srgrusso <srgrusso240@gmail.com>
2024-03-09 14:08:23 -0600

    Blissify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lemonadep.mk
+    $(LOCAL_DIR)/bliss_lemonadep.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lemonadep-user \
-    lineage_lemonadep-userdebug \
-    lineage_lemonadep-eng
+    bliss_lemonadep-user \
+    bliss_lemonadep-userdebug \
+    bliss_lemonadep-eng
